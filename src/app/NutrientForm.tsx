639246b4c4e58f102c6a1e224f105bcb0320c3d9
Hamir Mahal <hamirmahal@gmail.com>
2024-03-07 01:35:12 -0800

    fix: incorrect `label` text for `input`

--- src/app/NutrientForm.tsx
+++ src/app/NutrientForm.tsx
@@ -66,7 +66,7 @@ const NutrientForm: React.FC = () => {
             htmlFor="foodDataCentralId"
             className="mb-2 block cursor-pointer font-medium text-gray-700 dark:text-gray-300"
           >
-            Enter 6-digit FoodData Central ID:
+            Enter FoodData Central ID:
           </label>
           <input
             className="w-full rounded-md border px-4 py-2 focus:border-blue-300 focus:outline-none focus:ring dark:bg-gray-800 dark:text-gray-100"
